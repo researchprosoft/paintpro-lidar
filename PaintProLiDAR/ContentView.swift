00012382cab0b9418dbfaf56f3f2dd8eb717d9f8
Brian Hout <brianh@opqo.co>
2026-03-29 14:35:10 -0700

    Initial Commit

---
+++ PaintProLiDAR/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PaintProLiDAR
+//
+//  Created by Donatello on 3/29/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PaintProLiDAR/PaintProLiDARApp.swift
@@ -0,0 +1,17 @@
+//
+//  PaintProLiDARApp.swift
+//  PaintProLiDAR
+//
+//  Created by Donatello on 3/29/26.
+//
+
+import SwiftUI
+
+@main
+struct PaintProLiDARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
